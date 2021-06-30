5ee7fb0f548f1c06231e554db4831370bb9c63c4
jerseywonder <work@andyball.info>
2021-06-30 12:00:15 +1000

    Update NSW table so it is using the same feed as the NSW hotspot map

--- atoms/default/client/js/app.js
+++ atoms/default/client/js/app.js
@@ -9,19 +9,66 @@ import groupBy from "shared/js/groupBy"
 // Very handy stuff in here
 // https://stackoverflow.com/questions/14446511/most-efficient-method-to-groupby-on-an-array-of-objects
 
+// https://interactive.guim.co.uk/embed/aus/feeds/covid/nsw-hotspots.json
+//https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/covid-case-locations-20210104b.json
+
+fetch(`https://interactive.guim.co.uk/embed/aus/feeds/covid/nsw-hotspots.json?t=${new Date().getTime()}`)
+    .then(res => res.json())
+    .then(json => {
+
+
+    	var results = json.map(item => {
+    		return { 
+    			"Suburb" : item.Suburb,
+    			"Venue" : `${item.Venue} <br/>${item.Address}`,
+    			"Date and time" : `${item.Time} on ${item.Date}`,
+    			"Health advice" : (item.HealthAdviceHTML!=undefined) ? item.HealthAdviceHTML.replace(/(<([^>]+)>)/gi, "") : "" 
+    		}
+    	})
+
+        var groups = groupBy(results, ['Suburb', 'Venue', 'Health advice'], "Date and time" );
+
+        for (const group of groups) {
+
+            var datelist = results.map(item => {
+
+                if (group.Suburb === item.Suburb && group.Venue === item.Venue && group["Health advice"] === item["Health advice"]) {
+
+                    return item["Date and time"]
+
+                }
+
+            })
+
+            var filtered = datelist.filter(Boolean);
+
+            group["Date and time"] = filtered.join(', ');
+
+        }
+
+        console.log(groups.length)
+
+        data.sheets.data = groups.sort((a,b) => (a.Suburb > b.Suburb) ? 1 : ((b.Suburb > a.Suburb) ? -1 : 0)); 
+
+    	new Hotspots(data, "data")
+
+    });
+
+
+/*
 fetch(`https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289/resource/f3a28eed-8c2a-437b-8ac1-2dab3cf760f9/download/covid-case-locations-20210104b.json?t=${new Date().getTime()}`)
     .then(res => res.json())
     .then(json => {
 
 
-    	var results = json.data.monitor.map(item => {
-    		return { 
-    			"Suburb" : item.Suburb,
-    			"Venue" : item.Venue,
-    			"Date and time" : `${item.Time} on ${item.Date}`,
-    			"Health advice" : (item.HealthAdviceHTML!=undefined) ? item.HealthAdviceHTML.replace(/(<([^>]+)>)/gi, "") : "" 
-    		}
-    	})
+        var results = json.data.monitor.map(item => {
+            return { 
+                "Suburb" : item.Suburb,
+                "Venue" : item.Venue,
+                "Date and time" : `${item.Time} on ${item.Date}`,
+                "Health advice" : (item.HealthAdviceHTML!=undefined) ? item.HealthAdviceHTML.replace(/(<([^>]+)>)/gi, "") : "" 
+            }
+        })
 
         var groups = groupBy(results, ['Suburb', 'Venue', 'Health advice'], "Date and time" );
 
@@ -49,6 +96,7 @@ fetch(`https://data.nsw.gov.au/data/dataset/0a52e6c1-bc0b-48af-8b45-d791a6d8e289
 
         console.log(data)
 
-    	new Hotspots(data, "data")
+        new Hotspots(data, "data")
 
     });
+*/
